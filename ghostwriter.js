cfa571e1f2a052f08f012edbefc5fbd45e366c9c
Anna Schuplinsky <aschuplinsky@mail.csuchico.edu>
2017-04-10 08:16:17 -0700

    tried to make a random sentence

--- ghostwriter.js
+++ ghostwriter.js
@@ -1,10 +1,19 @@
 function writeTweet()
 {
-  
+  var randIndex=randomUpTo();
+  var sentence = intro[randIndex];
+  var intro = ["The mind is", "God is ", "The meaning of life is", "Only you are" ];
+  var body = ["more powerful than you can imagine ", "complex and cannot be truly understood ", "the best thing ever ", "unimaginably creative ", "crazy and weird ", "boring and too complex "];
+  var end = ["so stop wasting your time trying to understand it.", "therefore, you should never give up on your dreams.", "so get out there and make some memories."];
 }
 
 //Generates a random whole number between 0 and max (inclusive)
-function randomUpTo(max)
+function randomUpTo(4)
 {
-  return Math.floor(Math.random() * (max+1));
+  return=Math.floor(Math.random() * (intro.length+1));
+  return=Math.floor(Math.random() * (body.length+1));
+  return=Math.floor(Math.random() * (end.length+1));
+
+  document.write(intro[randIndex] + " " + body[randIndex] + " " + end[randIndex]) + ".").innerHTML;
+
 }
